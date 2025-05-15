b48f9096bad2cd5efafe1566cbc756c80f3e7684
Corentin Lallement <corentinlallement5@gmail.com>
2025-05-16 01:39:50 +0200

    fix: install_ntp.sh

--- install_ntp.sh
+++ install_ntp.sh
@@ -9,7 +9,7 @@ echo "[NTP] Activation et démarrage du service"
 sudo systemctl enable --now chronyd
 
 echo "[NTP] Configuration du serveur pour fournir l'heure aux autres machines"
-sudo sed -i 's/^#allow.*/allow 10.42.0.0\\/16/' /etc/chrony.conf
+sudo sed -i 's|^#allow.*|allow 10.42.0.0/16|' /etc/chrony.conf
 
 echo "[NTP] Redémarrage du service chronyd"
 sudo systemctl restart chronyd
