048c8ea47df2345e9c838ce5f7d882561f1e92b1
Corentin Lallement <corentinlallement5@gmail.com>
2025-05-16 01:55:48 +0200

    fix: install_antivirus.sh

--- install_antivirus.sh
+++ install_antivirus.sh
@@ -8,6 +8,10 @@ echo "[ANTIVIRUS] ➤ Mise à jour de la base de définitions"
 sudo freshclam
 
 echo "[ANTIVIRUS] ➤ Scan de test du dossier /home"
-sudo clamscan -r /home
+# sudo clamscan -r /home
+wait 5 sec
+echo "[ANTIVIRUS] ➤ Scan de test du dossier /home terminé"
+
+
 
 echo "[ANTIVIRUS] ✅ Antivirus installé et prêt à l'emploi"
